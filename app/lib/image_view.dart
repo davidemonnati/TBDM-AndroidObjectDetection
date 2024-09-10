2bdf71166899cebd47063865473a61a4b458420d
davidemonnati <davide.monnati@gmail.com>
2024-09-10 22:18:41 +0200

    add loading spinner

--- app/lib/image_view.dart
+++ app/lib/image_view.dart
@@ -5,10 +5,18 @@ import 'package:app/service/client_service.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class ImageView extends StatelessWidget {
+class ImageView extends StatefulWidget {
+  const ImageView({super.key, required this.image});
   final XFile image;
 
-  const ImageView({super.key, required this.image});
+  State<ImageView> createState() => _ImageViewState(image: image);
+}
+
+class _ImageViewState extends State<ImageView> {
+  _ImageViewState({required this.image});
+
+  final XFile image;
+  static bool loading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +25,44 @@ class ImageView extends StatelessWidget {
         body: Column(
           children: [
             Image.file(File(image.path)),
-            ElevatedButton(child: const Text("Send"), onPressed: () {
-              Future<ImageProvider> imageWithDetections = uploadImage(
-                  File(image.path));
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) =>
-                      ResponseView(image: imageWithDetections))
-              );
-            }),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 24),
+                  minimumSize: const Size.fromHeight(55),
+                  shape: const StadiumBorder(),
+                  backgroundColor: Colors.lightBlue,
+                ),
+                onPressed: () {
+                  setState(() => loading = true);
+                  uploadImage(File(image.path)).then((image) {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) => ResponseView(image: image))
+                    );
+                    setState(() => loading = false);
+                  }).onError((e, s) {
+                    setState(() => loading = false);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: const Text(
+                                "A connection error occurred"),
+                            action: SnackBarAction(
+                              label: 'Close',
+                              onPressed: () {},
+                            )
+                        )
+                    );
+                  });
+                },
+                child: loading ? const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(color: Colors.white),
+                    SizedBox(width: 24),
+                    Text("Processing image..."),
+                  ],
+                ) : const Text("Send")
+            ), //SnackBar(content: content)
           ],
         )
     );

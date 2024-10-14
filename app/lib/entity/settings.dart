4d710ef4bbf1696f225316d8fd94cf26f4d82c4f
davidemonnati <davide.monnati@gmail.com>
2024-10-14 22:33:42 +0200

    remove id attribute

--- app/lib/entity/settings.dart
+++ app/lib/entity/settings.dart
@@ -1,17 +1,14 @@
 class Settings {
-  final int id;
   final String detectionIp;
   final String storageIp;
 
   const Settings({
-    required this.id,
     required this.detectionIp,
     required this.storageIp
   });
 
   Map<String, Object?> toMap() {
     return {
-      'id': id,
       'detection_ip': detectionIp,
       'storage_ip': storageIp,
     };

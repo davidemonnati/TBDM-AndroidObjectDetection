b738d5ab6777971a1684fab9983d1272cf9f297b
davidemonnati <davide.monnati@gmail.com>
2024-10-16 18:28:51 +0200

    refactoring network_manager and network_manager_interface

--- app/lib/service/network_manager/network_manager.dart
+++ app/lib/service/network_manager/network_manager.dart
@@ -6,8 +6,7 @@ import 'dart:typed_data';
 import 'package:app/entity/detection/result.dart';
 import 'package:app/exception/app_error.dart';
 import 'package:app/exception/app_exception.dart';
-import 'package:app/service/network_manager_interface.dart';
-import 'package:app/util/constants.dart';
+import 'package:app/service/network_manager/network_manager_interface.dart';
 import 'package:http/http.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -15,7 +14,7 @@ class NetworkManager implements INetworkManager {
 
   @override
   Future<Result> uploadImage(String uri, File image) {
-    Future<StreamedResponse> response = _makeHttpRequest(uri, image)
+    Future<StreamedResponse> response = _makeHttpRequest("$uri/", image)
         .catchError((error) {
       throw AppException(AppError.connectionNotAvailable);
     });
@@ -23,15 +22,15 @@ class NetworkManager implements INetworkManager {
   }
 
   @override
-  Future<File> getElaboratedImage(Result result) {
+  Future<File> getElaboratedImage(String uri, Result result) {
     String uuid = result.uuid;
-    return _getImage(Constants.uri, uuid);
+    return _getImage(uri, uuid);
   }
 
   @override
-  Future<StreamedResponse> saveImage(File image, String json) async {
+  Future<StreamedResponse> saveImage(String uri, File image, String json) async {
     var request = MultipartRequest(
-        'POST', Uri.parse("${Constants.saveImageUri}app/image"));
+        'POST', Uri.parse("${uri}/app/image"));
     request.files.add(MultipartFile.fromBytes(
         'image',
         File(image.path).readAsBytesSync(),
@@ -66,7 +65,7 @@ class NetworkManager implements INetworkManager {
   }
 
   Future<File> _getImage(String uri, String uuid) async {
-    var response = await get(Uri.parse("${uri}get-image/${await uuid}"));
+    var response = await get(Uri.parse("${uri}/get-image/${await uuid}"));
     Uint8List uint8list = response.bodyBytes;
     ByteBuffer buffer = uint8list.buffer;
     ByteData byteData = ByteData.view(buffer);
--- app/lib/service/network_manager/network_manager_interface.dart
+++ app/lib/service/network_manager/network_manager_interface.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart';
 abstract class INetworkManager {
   Future<Result> uploadImage(String uri, File image);
 
-  Future<File> getElaboratedImage(Result result);
+  Future<File> getElaboratedImage(String uri, Result result);
 
-  Future<StreamedResponse> saveImage(File image, String json);
+  Future<StreamedResponse> saveImage(String uri, File image, String json);
 }
